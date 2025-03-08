075d33c2c57c7c93be30e069d4492e5dddc85d32
Morghee <morgane144.morgane@gmail.com>
2025-03-08 15:29:33 +0100

    3 commit

--- resultats.py
+++ resultats.py
@@ -1,6 +1,6 @@
 resultats =  {
-    "sonic": "Tu es Sonic, le héros de cette série !",
-    "amy": "Tu es Amy.",
+    "sonic": "C'est le principal protagoniste et leader de Sonic Boom. Sonic est un hérisson adolescent bleu capable de courir à des vitesses supersoniques. Bien qu’il ait un bon cœur et de bonnes intentions et qu’il soit très bienveillant, Sonic peut être plutôt impatient. Il apprécie également son statut de héros et n’aime pas la compétition.",
+    "amy": "Un hérisson rose et adolescent, Amy est le membre le plus énergique de la Team Sonic et la meilleure amie de Sticks. Elle sert d’organisatrice et d’archéologue de l’équipe, et manie un marteau géant, qui est son bien le plus précieux, au combat.",
     "dr": "Tu es Doctor Eggman, l'antagoniste de cette série !",
-    "tails": "Tu es Tails, l'ingénieux du groupe !"
+    "tails": "Un jeune renard jaune, Tails est l’acolyte de Sonic. Tails est capable d'utiliser ses 2 queues pour réaliser le vol comme un hélicoptère. Il porte des lunettes et une ceinture à outils, et sert de mécanicien, de pilote et d’expert en technologie de l’équipe Sonic. Confiant en ses capacités, Tails passe une grande partie de son temps à bricoler des inventions destinées à faciliter la vie, bien qu’elles ne se déroulent pas toujours comme prévu. !"
   }
